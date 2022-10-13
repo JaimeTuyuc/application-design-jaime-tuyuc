01958283fff77f0448a2caa5d92facdd4349d06b
Jaime Tuyuc <jaimetuyuc7@gmail.com>
2022-10-13 13:45:23 -0600

    prueba deberia de fallar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
+import TestPage from './components/test';
 
 const App = () => {
   return (
     <div className="App">
       <h1>Hello Application design</h1>
-    </div>
 
-  )
-}
+      <TestPage />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TestPage = () => {
+  return (
+    <>
+      <div>Hola Mundo</div>
+    </>
+  );
+};
+
+export default TestPage;
